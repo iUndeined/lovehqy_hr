ffd6d752188ea447fe7259814d2059aaa62ce0c7
iUndeined <869535088@qq.com>
2016-06-04 13:50:21 +0800

    新增全局状态常量类

---
+++ src/src/cjd/love/hqy/hr/pojo/S.java
@@ -0,0 +1,19 @@
+package cjd.love.hqy.hr.pojo;
+
+public final class S {
+	
+	public final static class Employee {
+		
+		/**
+		 * 员工状态 离职
+		 */
+		public final static int STATUS_GONE = 0;
+		
+		/**
+		 * 员工状态 在职
+		 */
+		public final static int STATUS_WORK = 1;
+		
+	}
+	
+}
