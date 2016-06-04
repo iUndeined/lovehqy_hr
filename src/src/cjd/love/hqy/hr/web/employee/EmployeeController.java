b9186560efb972e77659ac113c6e1d169ffde598
iUndeined <869535088@qq.com>
2016-06-04 13:51:00 +0800

    雇员页面结构调整

--- src/src/cjd/love/hqy/hr/web/employee/EmployeeController.java
+++ src/src/cjd/love/hqy/hr/web/employee/EmployeeController.java
@@ -3,18 +3,36 @@ package cjd.love.hqy.hr.web.employee;
 import java.util.Date;
 import java.util.List;
 import cjd.love.hqy.hr.pojo.Employee;
+import cjd.love.hqy.hr.pojo.S;
 import cjd.love.hqy.hr.ext.HrLoveController;
+import cjd.love.hqy.hr.model.commons.AjaxResult;
 
 public class EmployeeController extends HrLoveController {
 	
 	public void join(){
 		// 获取 雇员
 		Employee employee = this.getModel(Employee.class, "e");
-		// 添加
+		employee.setStatus(S.Employee.STATUS_WORK);
 		employee.setCreatedDate(new Date());
 		employee.save();
 		// 返回 成功
 		this.success();
+	}
+	
+	public void gone(){
+		// 获取 雇员id
+		Integer emplId = this.getParaToInt("id");
+		if (emplId == null) {
+			this.renderJson(new AjaxResult("雇员id不能为空"));
+			return;
+		}
+		// 更新 雇员状态
+		Employee employee = Employee.dao.findById(emplId);
+		employee.setStatus(S.Employee.STATUS_GONE);
+		employee.update();
+		
+		// 返回成功
+		this.renderJson(new AjaxResult(true));
 	}
 	
 	public void list(){
