f64a4488bcc72aa0ae219aca54bf01d923107726
iUndeined <869535088@qq.com>
2016-06-08 14:54:47 +0800

    [+]新增员工列表查询类型

--- src/src/cjd/love/hqy/hr/pojo/S.java
+++ src/src/cjd/love/hqy/hr/pojo/S.java
@@ -4,6 +4,14 @@ public final class S {
 	
 	public final static class Employee {
 		
+		public final static class List {
+			
+			public final static int WORK = 0;
+			public final static int FIRE = 1;
+			public final static int ALL = 2;
+			
+		}
+		
 		/**
 		 * 员工状态 离职
 		 */
